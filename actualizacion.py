ac4f93a0009aa608c8fcb62c542cd61648f08ab4
Mauriciolaynes <mauriciolaynescastro@gmail.com>
2023-09-11 16:49:46 -0300

    first commit

---
+++ actualizacion.py
@@ -0,0 +1,8 @@
+import selenium
+from selenium import webdriver
+
+# Obtener la versión de Selenium
+print("Versión de Selenium:", selenium.__version__)
+
+# Obtener la versión del WebDriver
+print("Versión del WebDriver:", webdriver.__version__)
